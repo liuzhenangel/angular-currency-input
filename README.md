37fb44d9431a4ff26185e2dd7c217d4993f7bb45
liuzhenangel <550686383@qq.com>
2016-08-26 19:40:39 +0800

    add src & README

---
+++ README.md
@@ -0,0 +1,8 @@
+## Quick start
+
++ Install AngularStrap with [Bower](https://github.com/bower/bower).
+
+>
+```bash
+$ bower install angular-strap --save
+```
---
+++ src/currency_input.js
@@ -0,0 +1,47 @@
+angular.directive('currencyInput', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      field: '='
+    },
+    replace: true,
+    template: '<input type="text"></input>',
+    link: function(scope, element, attrs) {
+      var doWithDecimal, input, updateInputValue;
+      doWithDecimal = function(inputVal) {
+        if (!inputVal) {
+          return '0';
+        }
+        if (typeof inputVal === 'string') {
+          inputVal = inputVal.replace(/[^\d.\',']/g, '');
+          inputVal = inputVal.replace(/,/g, '');
+          inputVal = inputVal.substring(0, 15);
+        }
+        return new BigNumber(inputVal).toFormat();
+      };
+      input = element;
+      updateInputValue = function(inputVal) {
+        var res;
+        res = doWithDecimal(inputVal);
+        console.log('xxxx:' + res);
+        input.val(res);
+        return input.data('origin-val', res.replace(/,/g, ''));
+      };
+      scope.$watch('field', function() {
+        var inputVal;
+        inputVal = scope.field;
+        updateInputValue(inputVal);
+      });
+      $(element).bind('keyup', (function(_this) {
+        return function(e) {
+          var inputVal;
+          inputVal = input.val();
+          updateInputValue(inputVal);
+          scope.$apply(function() {
+            scope.field = Number(input.data('origin-val'));
+          });
+        };
+      })(this));
+    }
+  };
+});
